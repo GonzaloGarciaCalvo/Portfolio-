b66b41a0929790970cfb712e9273a3b877cdf417
Gonzalo <garciacalvog@hotmail.com>
2022-05-04 12:17:32 -0300

    comprimo imagenes, ready jquery

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
+$(document).ready(function () {
+	$("#subir").click(function (e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, 400); //obtenemos el alto del documento
+	});
 
-$('#subir').click(function (e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, 400); //obtenemos el alto del documento
-})
-
-$('#misContactos').click(function () {
-    $('body,html').animate({scrollTop: $(document).height()}, 17); //obtenemos el alto del documento
-})
+	$("#misContactos").click(function () {
+		$("body,html").animate({ scrollTop: $(document).height() }, 17); //obtenemos el alto del documento
+	});
+});
